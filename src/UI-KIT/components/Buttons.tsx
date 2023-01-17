4bc5dfe0c6ecfc9658d48932d46b61196986391d
melanie-hugues <melanie.hugues@etu.univ-amu.fr>
2023-01-17 17:08:08 +0100

    feat: création des pages conditions utilisateurs et mention légales

--- src/UI-KIT/components/Buttons.tsx
+++ src/UI-KIT/components/Buttons.tsx
@@ -1,16 +1,46 @@
 import React from "react";
 import styled from '@emotion/styled';
+import { ThemeProvider } from '@emotion/react'
 import { useState } from "react";
+/*const theme = {
+    primary: {
+        default: "#053C5E",
+        borderDefault: "none",
+        colorDefault: "#FFFFFF",
+        hover: "#064874",
+        hoverColor: "none",
+        hoverBorder: "none"
+    },
+
+    primaryOutlined: {
+        default: "none",
+        borderDefault: "#053C5E",
+        colorDefault: "#053C5E",
+        hover: "#075688",
+        hoverColor: "#FFFFFF",
+        hoverBorder: "none",
+    },
+
+    secondary: {
+        default: "#BFDBF7",
+        borderDefault: "#053C5E",
+        colorDefault: "#053C5E",
+        hover: "#BFDBF7",
+        hoverColor: "none",
+        hoverBorder: "#053C5E"
+    }
+};
 
 
-const StylePrim1 = styled.button`
+const Style = styled.button<{theme: string}>`
     display: inline-block;
     width: 150px;
     height: 55.05px;
     left: 33px;
     top: 12px;
 
-    background: #053C5E;
+    background-color: ${(props) => props.theme.default};
+    border: 1px solid ${(props) => props.theme.borderDefault};
     border-radius: 8px;
 
     font-family: 'Poppins';
@@ -22,84 +52,16 @@ const StylePrim1 = styled.button`
     justify-content: center;
     align-items: center;
     text-align: center;
-    color: #FFFFFF;
+    color: ${(props) => props.theme.colorDefault};
     cursor: pointer;
     border : none;
 
     box-shadow: 0px 2px 2px lightgray;
     transition: ease background-color 250ms;
     &:hover {
-        background-color: #064874
-    }
-    &:disabled {
-        cursor: pointer;
-        opacity: 0.7;
-    }
-`
-
-const StylePrim2 = styled.button`
-    display: inline-block;
-    width: 150px;
-    height: 55.05px;
-    left: 33px;
-    top: 12px;
-
-    border: 1px solid #053C5E;
-    border-radius: 8px;
-
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    color: #053C5E;
-    cursor: pointer;
-
-    box-shadow: 0px 2px 2px lightgray;
-    transition: ease background-color 250ms;
-    &:hover {
-        color: #FFFFFF;
-        background-color: #075688
-    }
-    &:disabled {
-        cursor: pointer;
-        opacity: 0.7;
-    }
-`
-
-const StyleSecon = styled.button`
-    display: inline-block;
-    width: 150px;
-    height: 55.05px;
-    left: 33px;
-    top: 12px;
-
-    background: #BFDBF7;
-    border: 1px solid #053C5E;
-    border-radius: 8px;
-
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    color: #053C5E;
-    cursor: pointer;
-
-    box-shadow: 0px 2px 2px lightgray;
-    transition: ease background-color 250ms;
-    &:hover {
-        background: #BFDBF7;
-        border: 1px solid #053C5E;
-        border-radius: 8px;
+        background-color: ${(props) => props.theme.hover};
+        color: ${(props) => props.theme.hoverColor};
+        border: 1px solid ${(props) => props.theme.hoverBorder};
     }
     &:disabled {
         cursor: pointer;
@@ -108,13 +70,14 @@ const StyleSecon = styled.button`
 `
 
 export const Primary = () => {
-    return <StylePrim1>Button</StylePrim1>
+    return <Style theme="primary">Button</Style>
 }
 
 export const PrimaryOutlined = () => {
-    return <StylePrim2>Button</StylePrim2>
+    return <Style theme="primaryOutlined">Button</Style>
 }
 
 export const Secondary = () => {
-    return <StyleSecon>Button</StyleSecon>
+    return <Style theme="secondary">Button</Style>
 }
+*/
