7f66a20524e9768273c5a4455b74bbd2f5c11965
melanie-hugues <melanie.hugues@etu.univ-amu.fr>
2023-01-18 15:29:07 +0100

    Intégration des deux pages de Condition Utilisateurs et de mention légales + Correction de la page mention légales

--- src/features/mentionLeg/MentionLeg.tsx
+++ src/features/mentionLeg/MentionLeg.tsx
@@ -44,7 +44,7 @@ const MentionLeg = () => {
 
                 <h2>Limitations contractuelles sur les données techniques</h2>
                 
-                <a>Le site utilise la technologie React JS et Symfony.
+                <a>Le site utilise la technologie React, node JS et Symfony.
 
                 Le site Internet ne pourra être tenu responsable de dommages matériels liés à l’utilisation du site. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour
                 </a>
