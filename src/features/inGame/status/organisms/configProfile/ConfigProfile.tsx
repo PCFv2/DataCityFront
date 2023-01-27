8cea06c510abb871971cd2ace771292a7f406496
HisokaBluee <mattias.gervilliers@etu.univ-amu.fr>
2023-01-27 08:41:01 +0100

    feat: config profile page

--- src/features/inGame/status/organisms/configProfile/ConfigProfile.tsx
+++ src/features/inGame/status/organisms/configProfile/ConfigProfile.tsx
@@ -13,6 +13,10 @@ import userApi, {
     useUpdateUserByIdMutation,
 } from "src/services/queries/user";
 import styled from "@emotion/styled";
+import background from "src/assets/img/inGame/backgrounds/night.webp";
+import renderIcon from "./molecules/RenderIcon";
+import RenderIcon from "./molecules/RenderIcon";
+import {keyframes} from "@emotion/react";
 
 type ConfigProfileProps = {
     handleFinishRound?: (
@@ -93,72 +97,120 @@ const ConfigProfile = (props: ConfigProfileProps) => {
     if (isLoading || isLoadingUserConfiguration || userLoading)
         return <OverlayLoader/>;
 
+    const ConfigPage = styled.main`
+      background: url(${background}) no-repeat center center fixed;
+      background-size: cover;
+      padding-bottom: 5rem;
+    `
+
     const Window = styled.div`
+      margin: auto;
       border-radius: ${(props) => props.theme.radius.medium};
       background-color: ${(props) => props.theme.colors.primary.white};
       display: flex;
       flex-direction: column;
+      width: 65%;
+      box-shadow: ${(props) => props.theme.colors.primary.black} 0px 7px 29px 0px;
     `
 
     const WindowTitle = styled.h1`
       color: ${(props) => props.theme.colors.primary.blue};
       font-size: 1.5rem;
       text-align: center;
-
+      margin-bottom: 0;
     `
 
     const Points = styled.p`
-
+      text-align: center;
     `
 
     const Table = styled.table`
-
+      background-color: ${(props) => props.theme.colors.primary.blue};
+      color: ${(props) => props.theme.colors.primary.white};
+      border-radius: 0 0 ${(props) => props.theme.radius.medium} ${(props) => props.theme.radius.medium};
+      border-collapse: collapse;
+      text-align: center;
+      vertical-align: center;
+      width: 100%;
+      overflow: scroll;
     `
 
     const TableLine = styled.tr`
-      border: white;
+      border: #FFF;
     `
 
     const TableHeader = styled.th`
+      border-bottom: ${(props) => props.theme.border.regular} ${(props) => props.theme.colors.primary.white};
+      padding: 1rem;
 
+      &:nth-child(1) {
+        border-right: ${(props) => props.theme.border.regular} ${(props) => props.theme.colors.primary.white};
+        border-bottom: none;
+      }
     `
 
     const TableHeadersLine = styled.tr`
-
     `
 
-    const TableElement = styled.td`
+    const TableElement = styled.td<{ isDisabled?: boolean }>`
 
+      padding: 0.5rem;
+
+      & svg {
+        width: 48px;
+        height: 48px;
+        padding: 4px;
+        cursor: pointer;
+        filter: grayscale(${(props) => props.isDisabled ? "1" : "0"});
+      }
+
+      & input {
+        display: none;
+
+        &:checked + label {
+          & svg {
+            border-radius: 50%;
+            border: solid 3px ${(props) => props.theme.colors.primary.white};
+          }
+        }
+      }
     `
 
+    const TableElementName = styled.td`
+      border-right: ${(props) => props.theme.border.regular} ${(props) => props.theme.colors.primary.white};
+    `
 
     const SaveButton = styled.button`
       background-color: unset;
       border: none;
       cursor: pointer;
       border-radius: 50%;
+
       & span {
         color: ${(props) => props.theme.colors.primary.lightBlue};
-        font-size: 48px;
+        font-size: 64px;
       }
     `
 
     return (
-        <Window>
-            <WindowTitle>Configuration de votre profil de jeu</WindowTitle>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Table>
-                    <TableHeadersLine>
-                        <TableHeader>0 points</TableHeader>
-                        <TableHeader>1 points</TableHeader>
-                        <TableHeader>2 points</TableHeader>
-                        <TableHeader>3 points</TableHeader>
-                    </TableHeadersLine>
+        <ConfigPage>
+            <Window>
+                <WindowTitle>Configuration de votre profil de jeu</WindowTitle>
+                <Points>Il vous reste <b>{displayPlayerPoints} points</b></Points>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Table>
+                        <TableHeadersLine>
+                            <TableHeader></TableHeader>
+                            <TableHeader>0 points</TableHeader>
+                            <TableHeader>1 points</TableHeader>
+                            <TableHeader>2 points</TableHeader>
+                            <TableHeader>3 points</TableHeader>
+                        </TableHeadersLine>
 
-                    {Object.values(allConfiguration!).map((elm: Configuration, index) => (
-                        <TableLine key={elm.configurationId}>
-                                <TableElement>
-                                    <label htmlFor={elm.value1}>{elm.value1}</label>
+                        {Object.values(allConfiguration!).map((elm: Configuration, index) => (
+                            <TableLine key={elm.configurationId}>
+                                <TableElementName>{elm.name}</TableElementName>
+                                <TableElement isDisabled={displayPlayerPoints < 0}>
                                     <input
                                         id={elm.value1}
                                         value={"value1"}
@@ -167,9 +219,10 @@ const ConfigProfile = (props: ConfigProfileProps) => {
                                         onClick={() => handleClick(index, elm.configurationId, 0)}
                                         {...register(`configuration.${index}.value`)}
                                     />
+                                    <label htmlFor={elm.value1} title={elm.value1}><RenderIcon
+                                        name={elm.value1}></RenderIcon></label>
                                 </TableElement>
-                                <TableElement>
-                                    <label htmlFor={elm.value2}>{elm.value2}</label>
+                                <TableElement isDisabled={displayPlayerPoints - 1 < 0}>
                                     <input
                                         id={elm.value2}
                                         value={"value2"}
@@ -178,9 +231,10 @@ const ConfigProfile = (props: ConfigProfileProps) => {
                                         onClick={() => handleClick(index, elm.configurationId, 1)}
                                         {...register(`configuration.${index}.value`)}
                                     />
+                                    <label htmlFor={elm.value2} title={elm.value2}><RenderIcon
+                                        name={elm.value2}></RenderIcon></label>
                                 </TableElement>
-                                <TableElement>
-                                    <label htmlFor={elm.value3}>{elm.value3}</label>
+                                <TableElement isDisabled={displayPlayerPoints - 2 < 0}>
                                     <input
                                         id={elm.value3}
                                         value={"value3"}
@@ -189,11 +243,12 @@ const ConfigProfile = (props: ConfigProfileProps) => {
                                         onClick={() => handleClick(index, elm.configurationId, 2)}
                                         {...register(`configuration.${index}.value`)}
                                     />
+                                    <label htmlFor={elm.value3} title={elm.value3}><RenderIcon
+                                        name={elm.value3}></RenderIcon></label>
                                 </TableElement>
                                 {elm.value4 && (
                                     <>
-                                        <TableElement>
-                                            <label htmlFor={elm.value4}>{elm.value4}</label>
+                                        <TableElement isDisabled={displayPlayerPoints - 3 < 0}>
                                             <input
                                                 id={elm.value4}
                                                 value={"value4"}
@@ -202,16 +257,24 @@ const ConfigProfile = (props: ConfigProfileProps) => {
                                                 onClick={() => handleClick(index, elm.configurationId, 3)}
                                                 {...register(`configuration.${index}.value`)}
                                             />
+                                            <label htmlFor={elm.value4} title={elm.value4}><RenderIcon
+                                                name={elm.value4}></RenderIcon></label>
                                         </TableElement>
                                     </>
                                 )}
-                        </TableLine>
-                    ))}
-                    <SaveButton type="submit"><span className="material-icons">check_circle</span></SaveButton>
-                </Table>
-            </form>
-            <Points>Il vous reste {displayPlayerPoints} points</Points>
-        </Window>
+                            </TableLine>
+                        ))}
+                        <TableElement></TableElement>
+                        <TableElement></TableElement>
+                        <TableElement></TableElement>
+                        <TableElement></TableElement>
+                        <TableElement>
+                            <SaveButton type="submit"><span className="material-icons">check_circle</span></SaveButton>
+                        </TableElement>
+                    </Table>
+                </form>
+            </Window>
+        </ConfigPage>
     );
 };
 
