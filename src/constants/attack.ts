754514c7a41f765a9861f453a10d2af131efc903
Marius <marius.pistoresi@etu.univ-amu.fr>
2023-01-27 10:00:45 +0100

    Re order attack

--- src/constants/attack.ts
+++ src/constants/attack.ts
@@ -2,8 +2,8 @@ export const ATTACK = {
     nbAttacks: 3,
     attacks: {
         phishing: 0,
-        eavesdropping: 1,
-        stealthDownload: 2,
+        stealthDownload: 1,
+        eavesdropping: 2,
         crossSiteScripting: 3,
         passwordCracking: 4
     }
