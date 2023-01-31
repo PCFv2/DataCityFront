e964ba55f4cda13cd413ff6f35e1a160be3450b0
GuillaumeKusiak <guillaume.kusiak@etu.univ-amu.fr>
2023-01-31 15:05:23 +0100

    fix: fix isDisabled for configprofil

--- src/services/queries/user.ts
+++ src/services/queries/user.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_URL_DEV + "/user",
+    baseUrl: process.env.REACT_APP_API_URL + "/user",
   }),
   tagTypes: ["user"],
   endpoints: (builder) => ({
