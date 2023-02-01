f45aec8f5b43b73bf89129ee343af13103392367
melanie-hugues <melanie.hugues@etu.univ-amu.fr>
2023-02-01 08:56:32 +0100

    fix: Correction  de la page legalMention

--- src/features/legalMention/legalMention.tsx
+++ src/features/legalMention/legalMention.tsx
@@ -41,7 +41,9 @@ const LegalMention = () => {
                 </a>
 
                 <a>Editeur : 
-                PFCv2 SARL
+                Dans le cadre d'un projet scolaire, un groupe d'élèves ont créé ce projet. Le groupe qui se nomme PCFv2 constituer des élèves suivant : PISTORESI Marius, GERVILLIERS Mattias, KUSIAK Guillaume, et HUGUES Mélanie
+
+                IUT Aix-Marseille
 
                 413 Av. Gaston Berger
 
@@ -66,7 +68,7 @@ const LegalMention = () => {
                 
                 <a>Le site data-city a pour objet de fournir une information concernant l’ensemble des activités de la société.
 
-                PCFv2 SARL s’efforce de fournir sur le site data-city des informations aussi précises que possible. Toutefois, il ne pourra être tenue responsable des omissions, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.
+                Le groupe PCFv2 s’efforce de fournir sur le site data-city des informations aussi précises que possible. Toutefois, il ne pourra être tenue responsable des omissions, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.
 
                 Tous les informations indiquées sur le site data-city sont données à titre indicatif, et sont susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site data-city ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne.
                 </a>
@@ -92,20 +94,20 @@ const LegalMention = () => {
 
                 <h2>Propriété intellectuelle et contrefaçons</h2>
                 
-                <a>PCFv2 SARL est propriétaire des droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, notamment les textes, images, graphismes, logo, icônes, sons, logiciels.
+                <a>Le groupe PCFv2 est propriétaire des droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, notamment les textes, images, graphismes, logo, icônes, sons, logiciels.
                 
-                Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de : PCFv2 SARL.
+                Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable du groupe PCFv2.
 
                 Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.
                 </a>
 
                 <h2>Limitations de responsabilité</h2>
                 
-                <a>PCFv2 SARL ne pourra être tenue responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site data-city, et résultant soit de l’utilisation d’un matériel ne répondant pas aux spécifications indiquées au point 4, soit de l’apparition d’un bug ou d’une incompatibilité.
+                <a>Le groupe PCFv2 ne pourra être tenue responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site data-city, et résultant soit de l’utilisation d’un matériel ne répondant pas aux spécifications indiquées au point 4, soit de l’apparition d’un bug ou d’une incompatibilité.
 
-                PCFv2 SARL ne pourra également être tenue responsable des dommages indirects (tels par exemple qu’une perte de marché ou perte d’une chance) consécutifs à l’utilisation du site data-city.
+                Le groupe PCFv2 ne pourra également être tenue responsable des dommages indirects (tels par exemple qu’une perte de marché ou perte d’une chance) consécutifs à l’utilisation du site data-city.
 
-                Des espaces interactifs (possibilité de poser des questions dans l’espace contact) sont à la disposition des utilisateurs. PCFv2 SARL se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données. Le cas échéant, PCFv2 SARL se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie…).
+                Des espaces interactifs (possibilité de poser des questions dans l’espace contact) sont à la disposition des utilisateurs. Le groupe PCFv2 se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données. Le cas échéant, Le groupe PCFv2 se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie…).
                 </a>
 
                 
@@ -119,7 +121,7 @@ const LegalMention = () => {
 
                 Conformément aux dispositions des articles 38 et suivants de la loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, tout utilisateur dispose d’un droit d’accès, de rectification et d’opposition aux données personnelles le concernant, en effectuant sa demande écrite et signée, accompagnée d’une copie du titre d’identité avec signature du titulaire de la pièce, en précisant l’adresse à laquelle la réponse doit être envoyée.
 
-                Aucune information personnelle de l’utilisateur du site data-city n’est publiée à l’insu de l’utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des tiers. Seule l’hypothèse du rachat de PCFv2 SARL et de ses droits permettrait la transmission des dites informations à l’éventuel acquéreur qui serait à son tour tenu de la même obligation de conservation et de modification des données vis à vis de l’utilisateur du site data-city.
+                Aucune information personnelle de l’utilisateur du site data-city n’est publiée à l’insu de l’utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des tiers. Seule l’hypothèse du rachat du groupe PCFv2 et de ses droits permettrait la transmission des dites informations à l’éventuel acquéreur qui serait à son tour tenu de la même obligation de conservation et de modification des données vis à vis de l’utilisateur du site data-city.
 
                 Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.
                 </a>
@@ -137,7 +139,7 @@ const LegalMention = () => {
                 
                 <a>Utilisateur : Internaute se connectant, utilisant le site susnommé.
 
-                Informations personnelles : « les informations qui permettent, sous quelque forme que ce soit, directement ou non, l’identification des personnes physiques auxquelles elles s’appliquent » (article 4 de la loi n° 78-17 du 6 janvier 1978).
+                Informations personnelles : aucune données ou informations personnelles seront récupérées par notre site.
                 </a>
             </body>
         </PageMl>
